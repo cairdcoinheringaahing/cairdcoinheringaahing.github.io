f6eee08e394febf820fb50473f2126c9b49c25af
cairdcoinheringaahing <valyrio.email@gmail.com>
2019-09-29 19:10:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 document.getElementById('run').onclick = function() {
 	document.enter = true;
-	console.log('Running...');
+	
+	document.size  =  document.getElementById('size');
+	document.limit =  document.getElementById('limit');
+	document.start = [document.getElementById('start_x'),
+			  document.getElementById('start_y'),
+			  document.getElementById('start_z')];
+	document.delta = [document.getElementById('delta_x'),
+			  document.getElementById('delta_y'),
+			  document.getElementById('delta_z')];
+	document.out   =  document.getElementById('out');
+	
+	document.flags = [document.getElementById('flag_b'),
+			  document.getElementById('flag_e'),
+			  document.getElementById('flag_x'),
+			  document.getElementById('flag_d')];
+	
+	document.input =  document.getElementById('input');
 };
